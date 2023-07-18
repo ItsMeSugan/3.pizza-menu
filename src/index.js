ff459576a5298e35aad832d3cb2a133a64058905
Sugan T <t.suganeeshwaran@gmail.com>
2023-07-18 13:03:24 +0530

    conditional rendering using ternery operator

--- src/index.js
+++ src/index.js
@@ -78,12 +78,14 @@ function Menu() {
     <main className="menu">
       <h2>Our Menu</h2>
 
-      {numPizzas > 0 && (
+      {numPizzas > 0 ? (
         <ul className="pizzas">
           {pizzas.map((pizza) => (
             <Pizza pizzaObj={pizza} key={pizza.name} />
           ))}
         </ul>
+      ) : (
+        <p>We are still working on our menu. Please come back later :)</p>
       )}
 
       {/* <Pizza
@@ -120,7 +122,7 @@ function Pizza(props) {
 
 function Footer() {
   const hour = new Date().getHours();
-  const openHour = 12;
+  const openHour = 2;
   const closeHour = 22;
   const isOpen = hour >= openHour && hour <= closeHour;
   console.log(isOpen);
@@ -130,11 +132,13 @@ function Footer() {
 
   return (
     <footer className="footer">
-      {isOpen && (
+      {isOpen ? (
         <div className="order">
           <p>We are open until {closeHour}:00. Come vist us or Order online</p>
           <button className="btn">Order</button>
         </div>
+      ) : (
+        <p>We are happy to welcome here..</p>
       )}
     </footer>
   );
